b6bd5fbed4933b0269a9433c5aafccbe87ea0d18
farruh <f0181642@gmail.com>
2024-07-02 20:46:13 +0500

    student bazasi tayor

---
+++ main.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+conn = psycopg2.connect(database="new_list",
+                        user="postgres",
+                        host="localhost",
+                        password="1",
+                        port="5432")
+
+cur = conn.cursor()
+
+select_all_students_query = """
+    select * from students;
+    """
+cur.execute(select_all_students_query)
+rows=cur.fetchall()
+for students in rows:
+    print (students)
